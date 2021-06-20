7a75a1b8a00b649446aec737c45a87620efa8dbd
Kris454 <kris454@wp.pl>
2021-06-20 18:06:15 +0200

    Add miniature

--- coins.gd
+++ coins.gd
@@ -9,7 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(0, 2)
+	position += Vector2(0, 1.7)
 	pass
 
 func _on_coins_body_entered(body):
